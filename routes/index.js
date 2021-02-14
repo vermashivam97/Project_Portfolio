8d30260a2d0cc418ba96f90e4cc357fef5f563f1
Shivam Verma <vermashivam1972@gmail.com>
2021-02-14 19:58:47 +0530

    Nav Bar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
+let express = require('express');
+let router = express.Router();
+let routingData = [
+	{
+		"name" : "Home",
+		"route" : "/"
+	},
+	{
+		"name" : "About",
+		"route" : "/"
+	},
+	{
+		"name" : "Services",
+		"route" : "/"
+	},
+	{
+		"name" : "Projects",
+		"route" : "/"
+	},
+	{
+		"name" : "Contact",
+		"route" : "/"
+	}
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+
+]
+
+
+//Url encode middleware
+
+router.use(express.urlencoded({extended:true}));
+
+//Home Page
+
+router.get('/',(req,res)=>{
+
+	res.render('home',{
+		title:"Portfolio",
+		navItems:routingData
+	});
+
 });
+
+
+router.get('/about',(req,res)=>{
+
+});
+
 
 module.exports = router;
